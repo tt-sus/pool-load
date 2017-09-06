ea7ddea35197a29ddf6ad5516b788765c0dd8398
akshar07 <takleakshar@gmail.com>
2017-09-06 15:06:25 -0400

    pool

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,5 @@ import { PoolComponent } from './pool/pool.component';
 import { ModuleWithProviders } from '@angular/core';
 
 export const appRoutes: Routes = [
- {path:"",loadChildren: './hot-water-savings/hot-water-savings.module#HotWaterSavingsModule'},
- {path:"pool-load",component:PoolComponent},
- { path: 'hc-savings', loadChildren: './hot-water-savings/hot-water-savings.module#HotWaterSavingsModule' }
+ {path:"",component:PoolComponent},
 ]
