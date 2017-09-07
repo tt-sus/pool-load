34fbba2b118f7ee75e6f6d153e6c22486b45eb0f
akshar07 <takleakshar@gmail.com>
2017-09-07 15:15:27 -0400

    image

--- src/app/pool/pool.component.ts
+++ src/app/pool/pool.component.ts
@@ -9,14 +9,14 @@ export class PoolComponent implements OnInit {
   heatsuppliedIp: number;
   airTemp: number = 100;
 waterTemp:number=90;
-waterTempCelcius:number=Math.round(((this.waterTemp-32)*5/9) * 100) / 100;;
-airTempCelcius:number=Math.round(((this.airTemp-32)*5/9) * 100) / 100;
+waterTempCelcius:number=Math.round(((this.waterTemp-32)*5/9) * 10) / 10;;
+airTempCelcius:number=Math.round(((this.airTemp-32)*5/9) * 10) / 10;
 width:number=100;
 length:number=80;
 depth:number=5.7;
-widthSI=this.width*0.3048;
-lengthSI=this.length*0.3048;
-depthSI=Math.round(this.depth*0.3048 * 100) / 100;
+widthSI=this.width*0.30;
+lengthSI=this.length*0.30;
+depthSI=Math.round(this.depth*0.3048 * 10) / 10;
 opHours:number=3000;
 refillFreq:number=2;
 heatLoss:number=5;
@@ -29,34 +29,34 @@ humidAir:number=0.0098;
 velocity:number=0.5;
 changeToSI(){
   this.widthSI=this.width*0.3048;
-  this.widthSI=Math.round(this.widthSI * 100) / 100;
+  this.widthSI=Math.round(this.widthSI * 10) / 10;
   this.lengthSI = this.length * 0.3048;
-  this.lengthSI=Math.round(this.lengthSI * 100) / 100;
+  this.lengthSI=Math.round(this.lengthSI * 10) / 10;
   this.depthSI = this.depth * 0.3048;
-  this.depthSI=Math.round(this.depthSI * 100) / 100;
+  this.depthSI=Math.round(this.depthSI * 10) / 10;
   this.calculate();
 }
 changeToSITemp(){
  this.airTempCelcius=(this.airTemp-32)*5/9;
-  this.airTempCelcius=Math.round(this.airTempCelcius * 100) / 100;
+  this.airTempCelcius=Math.round(this.airTempCelcius * 10) / 10;
   this.waterTempCelcius=(this.waterTemp-32)*5/9;
-  this.waterTempCelcius=Math.round(this.waterTempCelcius * 100) / 100;
+  this.waterTempCelcius=Math.round(this.waterTempCelcius * 10) / 10;
   this.calculate();
 }
 changeToIP(){
   this.width=3.280*this.widthSI;
-  this.width=Math.round(this.width * 100) / 100;
+  this.width=Math.round(this.width * 10) / 10;
   this.length=3.280*this.lengthSI;
-  this.length=Math.round(this.length * 100) / 100;
+  this.length=Math.round(this.length * 10) / 10;
   this.depth=3.280*this.depthSI;
-  this.depth=Math.round(this.depth * 100) / 100;
+  this.depth=Math.round(this.depth * 10) / 10;
   this.calculate();
 }
 changeToIPTemp(){
   this.airTemp=(this.airTempCelcius*1.8)+32;
-  this.airTemp=Math.round(this.airTemp * 100) / 100;
+  this.airTemp=Math.round(this.airTemp * 10) / 10;
   this.waterTemp=(this.waterTempCelcius*1.8)+32;
-  this.waterTemp=Math.round(this.waterTemp * 100) / 100;
+  this.waterTemp=Math.round(this.waterTemp * 10) / 10;
   this.calculate();
 }
 
